4a10a8650ce1b6f8beb24717b14219586549d531
Renan Fiuja <144910478+Fiujah@users.noreply.github.com>
2026-04-01 10:48:57 +0000

    add arquivo variaveis.js para teste no vs local conectado com vs web

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+let tipoLet = "Essa é uma variável do tipo let";
+const tipoConst = "Essa é uma variável do tipo Const";
+var tipoVar = "Essa é uma variável do tipo var";
+
+console.log(tipoLet);
+console.log(tipoConst);
+console.log(tipoVar);
